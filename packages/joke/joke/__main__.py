0720f02bac6c24c27b158a5c7a3357d68b9bcd2b
wwolff7 <wwolff7@gmail.com>
2022-08-30 14:20:15 -0300

    updating

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -1,13 +1,3 @@
-from pystac_client import Client
-import planetary_computer as pc
-from odc.stac import stac_load
-from math import floor
-import pandas as pd
-import geopandas as gpd
-from sqlalchemy import create_engine
-import psycopg2
-import os
-
 def main(args):
     name = args.get("name", "stranger")
     greeting = "Hello " + name + "!"

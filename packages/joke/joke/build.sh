f0e18312f6286ce882d5b22fe7835badfd759271
wwolff7 <wwolff7@gmail.com>
2022-08-30 15:01:27 -0300

    updating

--- packages/joke/joke/build.sh
+++ packages/joke/joke/build.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-virtualenv --without-pip virtualenv
-pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
+virtualenv virtualenv
+source virtualenv/bin/activate
+pip install -r requirements.txt
+deactivate
